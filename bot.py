7240cb3ba87fa793928baac118cc7f90243dbbf7
Caio Vinicius Marques Teixeira <marquest@usc.edu>
2018-10-24 19:35:42 -0300

    Add database

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram.ext import Updater
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import CommandHandler, Filters
 from telegram.error import (TelegramError, Unauthorized, BadRequest,
                             TimedOut, ChatMigrated, NetworkError)
+from model import Model
+
 import logging
 import os
 import feedparser
@@ -9,20 +11,35 @@ import feedparser
 from time import mktime
 from datetime import datetime, date, time
 
+
 def get_publish_date(post):
     parsed_date = post.published_parsed
     return datetime.fromtimestamp(mktime(parsed_date))
 
-def handle_link(bot, update):
-    d = feedparser.parse(update.message.text)
+
+def handle_update(bot, update):
+    feeds = model.get_all_feeds(update.message.chat_id)
+
+    for feed in feeds:
+        update_feed(bot, feed.chat_id, feed.url)
+
+
+def update_feed(bot, chat_id, feed):
+    d = feedparser.parse(feed)
 
     for post in d.entries:
         text = post.title
         text += '\t' + post.link
         publish_date = get_publish_date(post)
-        date_diff = datetime.utcnow() - publish_date;
-        if(date_diff.days < 1):
-            bot.send_message(chat_id=update.message.chat_id, text=text)
+        date_diff = datetime.utcnow() - publish_date
+        if (date_diff.days < 1):
+            bot.send_message(chat_id=chat_id, text=text)
+
+
+def handle_add_link(bot, update):
+    chat_id = update.message.chat_id
+    link = update.message.text
+    model.add_feed(chat_id, link)
 
 
 def error_callback(bot, update, error):
@@ -50,7 +67,10 @@ def error_callback(bot, update, error):
 
 def add_handlers(dispatcher):
     dispatcher.add_handler(
-        MessageHandler(Filters.text & (Filters.entity('url') | Filters.entity('text_link')), handle_link))
+        CommandHandler('add', handle_add_link))
+
+    dispatcher.add_handler(
+        CommandHandler('update', handle_update))
     dispatcher.add_error_handler(error_callback)
 
 
@@ -70,6 +90,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 add_handlers(dispatcher)
+
+model = Model()
 
 setup_webhook(updater, TOKEN)
 updater.idle()
---
+++ model.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+import os
+
+Base = declarative_base()
+
+
+class Chat(Base):
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(String(250), nullable=False)
+
+
+class Feed(Base):
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer, ForeignKey('chat.id'))
+    chat = relationship(Chat)
+    url = Column(String(500), nullable=False)
+
+
+class Model:
+    def __init__(self):
+        if not os.getenv("DATABASE_URL"):
+            raise RuntimeError("DATABASE_URL is not set")
+
+        self.engine = create_engine(os.getenv("DATABASE_URL"))
+        Base.metadata.bind = self.engine
+        db_session = sessionmaker(bind=self.engine)
+        self.session = db_session()
+
+    def get_chat(self, chat_id):
+        chat = self.session.query(Chat).filter(Chat.chat_id == chat_id).one_or_none()
+        if chat is not None:
+            chat = Chat(chat_id=chat_id)
+            self.session.add(chat)
+        return chat
+
+    def add_feed(self, chat_id, feed_url):
+        chat = self.get_chat(chat_id)
+        new_feed = Feed(chat=chat, url=feed_url)
+        self.session.add(new_feed)
+        self.session.commit()
+
+    def get_all_feeds(self, chat_id):
+        return self.session.query(Feed).filter(Feed.chat_id == chat_id).all()
